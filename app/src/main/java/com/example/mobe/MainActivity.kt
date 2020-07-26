f039acc68f76650518d9eb563975642fdb0ff88a
Aldhiya Rozak <32904416+Bejjoeqq@users.noreply.github.com>
2020-07-27 00:13:21 +0700

    1

---
+++ app/src/main/java/com/example/mobe/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mobe
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
